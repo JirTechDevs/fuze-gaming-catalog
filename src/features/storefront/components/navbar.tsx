bae7abc5ef40d4675d58ecde164b4b3ddb9930a8
dannnn512 <arya.ziddan7@gmail.com>
2026-05-07 17:00:23 +0700

    feat: restyle

--- src/features/storefront/components/navbar.tsx
+++ src/features/storefront/components/navbar.tsx
@@ -35,11 +35,8 @@ const socialLinks = [
 
 function WhatsAppGlyph() {
   return (
-    <svg viewBox="0 0 32 32" className="h-[18px] w-[18px]" aria-hidden="true">
-      <path
-        fill="currentColor"
-        d="M27.2 15.4c0 6.2-5 11.3-11.3 11.3-2 0-4-.5-5.7-1.5l-6.1 1.9 2-5.9a11.2 11.2 0 0 1-1.8-5.9C4.3 9.1 9.4 4 15.7 4S27.2 9.1 27.2 15.4Zm-11.5-9.5c-5.2 0-9.4 4.2-9.4 9.4 0 1.9.6 3.7 1.6 5.2l.2.2-1.2 3.7 3.8-1.2.2.1c1.5.9 3.2 1.4 4.9 1.4 5.2 0 9.4-4.2 9.4-9.4 0-5.2-4.2-9.4-9.5-9.4Zm5.4 12c-.3-.2-1.8-.9-2-1-.3-.1-.5-.2-.7.2-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1-.3-.2-1.3-.5-2.4-1.5-.9-.8-1.5-1.7-1.7-2-.2-.3 0-.4.1-.6l.5-.6c.2-.2.2-.4.3-.6.1-.2 0-.4 0-.6 0-.2-.7-1.7-.9-2.3-.2-.5-.5-.5-.7-.5h-.6c-.2 0-.6.1-.9.4-.3.3-1.1 1.1-1.1 2.6s1.1 3 1.3 3.2c.2.2 2.2 3.5 5.4 4.8 3.2 1.2 3.2.8 3.8.7.6-.1 1.8-.8 2.1-1.5.3-.7.3-1.4.2-1.5-.1-.1-.3-.2-.6-.4Z"
-      />
+    <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+      <path d="M17.6 6.32A7.85 7.85 0 0 0 12.05 4a7.94 7.94 0 0 0-6.88 11.9L4 20l4.2-1.1a7.93 7.93 0 0 0 3.84.98 7.94 7.94 0 0 0 7.94-7.93 7.88 7.88 0 0 0-2.39-5.63z" />
     </svg>
   );
 }
@@ -136,23 +133,6 @@ export default function Navbar() {
         </div>
 
         <div className={styles.navActions}>
-          {socialLinks.map((item) => {
-            const Icon = item.icon;
-
-            return (
-              <a
-                key={item.label}
-                href={item.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={item.label}
-                className={styles.navSocial}
-              >
-                <Icon />
-              </a>
-            );
-          })}
-
           <a
             href="https://wa.me/628881462675"
             target="_blank"
